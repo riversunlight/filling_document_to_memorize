bea99fd3fb92ac971abb7e11deb5b6d67a69f68f
riversunlight <aabbee107@icloud.com>
2023-01-02 19:36:55 +0900

    ファイルを読み込む機能を作成

--- filling_document_to_memorize/main.cpp
+++ filling_document_to_memorize/main.cpp
@@ -17,23 +17,44 @@ typedef struct _que {
 	vector<string> ans; // 穴埋めの答え
 }question;
 
+vector<question> question_data; // 問題データの管理
+
 // ファイルの情報を読み込む
 // 開いてから閉じるまで、一度読み込んだら暗記終了まで読み込まない
 void read_file() {
 	FILE* fp;
 	errno_t err;
 	char file_name[500];
+	char buf[1000];
+	int que_num = 0;
 
 	cout << "ファイル名を入力してください" << endl;
 	cin >> file_name;
 
 	err = fopen_s(&fp, file_name, "r");
-	if (err == EOF) {
+	if (err != 0) {
 	    cout << "ファイルオープンエラー" << endl;
 		return;
 	}
 
-
+	while (fgets(buf, 1000, fp) != NULL) {
+		string s;
+		s = buf;
+		if (s == "#E") {
+			que_num++;
+			continue;
+		}
+		if (s[0] == '#') {
+			s.erase(0, 2);
+			question_data[que_num].ans.push_back(s);
+		}
+		else {
+			question que;
+			que.doc = s;
+			question_data.push_back(que);
+		}
+	}
+	fclose(fp);
 }
 
 int main() {
